4d3cb72a3b676b24a371c17ecb4e91f78db29cb5
giorgiameffe <giorgiameffe03@gmail.com>
2025-02-27 19:50:48 +0100

    feat: add email exercise

--- js-email/main.js
+++ js-email/main.js
@@ -1,11 +1,37 @@
 
+// creare una lista di email di invitati ad una festa.
+
 const emailList = ['giorgia@gmail.com', 'margot@gmail.com','sabrina@gmail.com','tony@gmail.com'];
 
 console.log(emailList);
 
+// chiedere all'utente la sua email 
+
 const myEmail = prompt('Inserisci la tua email');
 
+let validEmail;
+
+// inizio ciclo for
+
 for (let i = 0; i < emailList.length; i++) {
+
+    // Controllare se l'email sia presente nella lista 
+
     let currentEmail = emailList[i];
-    console.log(currentEmail);    
+
+    // email valida
+
+    if (myEmail === currentEmail) {
+        console.log('Sei invitato, entra!');
+        validEmail = true;
+        break;
+    } 
+
 }
+
+// email non valida
+
+if (!validEmail) {
+    console.log('Non invitato');
+}
+
