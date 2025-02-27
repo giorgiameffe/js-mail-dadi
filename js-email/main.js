d051121687246874e6916d948df480ecd575f51f
giorgiameffe <giorgiameffe03@gmail.com>
2025-02-27 17:51:46 +0100

    feat: add first part

---
+++ js-email/main.js
@@ -0,0 +1,10 @@
+
+const emailList = ['giorgia@gmail.com', 'margot@gmail.com','sabrina@gmail.com','tony@gmail.com'];
+
+console.log(emailList);
+
+const myEmail = prompt('Inserisci la tua email');
+
+for (let i = 0; i < emailList.length; i++) {
+    
+}
