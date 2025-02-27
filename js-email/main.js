a160fdc41a2a5f290de9d5969430e6041f14b9c9
giorgiameffe <giorgiameffe03@gmail.com>
2025-02-27 18:03:41 +0100

    feat: add currentemail

--- js-email/main.js
+++ js-email/main.js
@@ -6,5 +6,6 @@ console.log(emailList);
 const myEmail = prompt('Inserisci la tua email');
 
 for (let i = 0; i < emailList.length; i++) {
-    
+    let currentEmail = emailList[i];
+    console.log(currentEmail);    
 }
