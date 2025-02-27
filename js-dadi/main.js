01ddcc80d2c7319776d459fee0db6832b7b9a412
giorgiameffe <giorgiameffe03@gmail.com>
2025-02-27 22:01:18 +0100

    feat: add random numbers

---
+++ js-dadi/main.js
@@ -0,0 +1,13 @@
+
+// generare un numero randomico per il computer
+
+const computerNumbers = Math.floor((Math.random()* 6) + 1);
+console.log(computerNumbers);
+
+// generare un numero randomico per l'utente 
+
+const userNumbers = Math.floor((Math.random()* 6) + 1);
+console.log(userNumbers);
+
+// se il numero generato è maggiore per il computer
+
