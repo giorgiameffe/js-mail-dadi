cdfa8703603bd8c0a9e6b99b28e56b7f88f493bf
giorgiameffe <giorgiameffe03@gmail.com>
2025-02-27 22:23:15 +0100

    feat: add if condition and comments

--- js-dadi/main.js
+++ js-dadi/main.js
@@ -11,3 +11,17 @@ console.log(userNumbers);
 
 // se il numero generato è maggiore per il computer
 
+if (computerNumbers > userNumbers) {
+    console.log(`Il computer ha generato il numero ${computerNumbers}, hai perso!`);
+    
+// se il numero generato è maggiore per l'utente
+
+} else if (userNumbers > computerNumbers) {
+    console.log(`Hai generato il numero ${userNumbers}, hai vinto!`);
+
+// se i numeri sono uguali
+    
+} else if (userNumbers === computerNumbers) {
+    console.log(`Pareggio!`);
+    
+}
